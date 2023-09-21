c9d3dedde8a6e8ee3145c34bcf0cb7162333a3ae
FDCrash <fd1crash@gmail.com>
2023-09-21 14:45:06 +0300

    Initial commit

---
+++ src/test/java/com/restaurant/collection/RestaurantCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
